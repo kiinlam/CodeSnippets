56cb6bf0d95916ce0787c11729108cd1a52e85a7
kiinlam <516523522@qq.com>
2020-12-22 15:02:59 +0800

    update

---
+++ [webpack]utils/require.context-common.js
@@ -0,0 +1,55 @@
+/**
+ * 使用webpack提供的require.context()方法, 将文件夹下的模块引入
+ * 参考链接
+ * https://webpack.js.org/guides/dependency-management/#requirecontext
+ * require.context() exports a (require) function that takes one argument: the request.
+ *
+ * The exported function has 3 properties: resolve, keys, id.
+ *
+ * resolve is a function and returns the module id of the parsed request.
+ * keys is a function that returns an array of all possible requests that the context module can handle.
+ * id is the module id of the context module. This may be useful for module.hot.accept
+ */
+
+/**
+ * 引入通用组件
+ * https://cn.vuejs.org/v2/guide/components-registration.html#%E5%9F%BA%E7%A1%80%E7%BB%84%E4%BB%B6%E7%9A%84%E8%87%AA%E5%8A%A8%E5%8C%96%E5%85%A8%E5%B1%80%E6%B3%A8%E5%86%8C
+ */
+
+import Vue from 'vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+const requireComponent = require.context(
+  // 其组件目录的相对路径
+  './components',
+  // 是否查询其子目录
+  false,
+  // 匹配基础组件文件名的正则表达式
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  // 获取组件配置
+  const componentConfig = requireComponent(fileName)
+
+  // 获取组件的 PascalCase 命名
+  const componentName = upperFirst(
+    camelCase(
+      // 获取和目录深度无关的文件名
+      fileName
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    )
+  )
+
+  // 全局注册组件
+  Vue.component(
+    componentName,
+    // 如果这个组件选项是通过 `export default` 导出的，
+    // 那么就会优先使用 `.default`，
+    // 否则回退到使用模块的根。
+    componentConfig.default || componentConfig
+  )
+})
