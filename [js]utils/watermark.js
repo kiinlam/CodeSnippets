641ae0e754368f75b619f0285b96a79de71148bf
kiinlam <516523522@qq.com>
2021-04-20 09:17:41 +0800

    update watermark

--- [js]utils/watermark.js
+++ [js]utils/watermark.js
@@ -1,17 +1,23 @@
 /**
  * 在指定的Dom元素内添加水印，可以多次调用来刷新水印
- * @param {string[]} textList 水印文字内容，每个数组元素对应一行，建议不超过3行 | default: []
- * @param {string} selector 要添加水印的选择器字符串 | default: body
- * @param {number} width 要添加水印的平铺瓦片高度 | default: 300
- * @param {number} height 要添加水印的平铺瓦片宽度 | default: 200
- * @param {number} fontsize 文字大小，单位px | default: 13
+ * @param {object} options 水印参数配置项
+ * @param {string[]} options.lines 水印文字内容，每个数组元素对应一行，建议不超过3行 | default: []
+ * @param {string} options.selector 要添加水印的选择器字符串 | default: body
+ * @param {number} options.width 要添加水印的平铺瓦片高度 | default: 300
+ * @param {number} options.height 要添加水印的平铺瓦片宽度 | default: 200
+ * @param {number} options.fontsize 文字大小，单位px | default: 13
+ * @param {number} options.rotate 水印倾斜角度，逆时针方向，单位度 | default: 45
+ * @param {string} options.color 水印颜色，canvas支持的颜色 | default: #e0e0e0
  * @example watermark(['Hello', 'world'])
- * @example watermark(['foo', 'bar'], '.watermark', 400, 300, 14)
+ * @example watermark(['foo', 'bar'], '.watermark', { width: 400, height: 300, fontsize: 16 })
  */
-export function watermark(textList = [], selector = 'body', width = 300, height = 200, fontsize = 13) {
+export function watermark(options = {}) {
+  const self = watermark
+  const { lines = [], selector = 'body', width = 300, height = 200, fontsize = 13, rotate = 45, color = '#e0e0e0' } = options
+
   // 没有文字内容时不需要设置水印
-  const lines = textList.length
-  if (lines === 0) return
+  const l = lines.length
+  if (l === 0) return
 
   // 创建临时canvas元素
   let canvas = document.createElement('canvas')
@@ -21,26 +27,29 @@ export function watermark(textList = [], selector = 'body', width = 300, height 
   // 绘制水印图
   let context = canvas.getContext('2d')
   const offset = fontsize * 1.9
-  const totalHeight = lines * offset
+  const totalHeight = l * offset
   context.translate(width / 2, height / 2)
-  context.rotate(-25 * Math.PI / 180)
+  context.rotate(-1 * rotate * Math.PI / 180)
   context.translate(0, -(totalHeight / 2))
-  context.font = `lighter ${fontsize}px "Microsoft YaHei", "PingFang SC", "SimSun"`
-  context.fillStyle = 'RGB(224, 224, 224)'
+  context.font = `lighter ${fontsize}px "STFangsong", "Microsoft YaHei", "PingFang SC", "SimSun"`
+  context.fillStyle = color
   context.textAlign = 'center'
-  textList.forEach((text, i) => {
+  lines.forEach((text, i) => {
     context.fillText(text, 0, i * offset)
   })
   context.setTransform(1, 0, 0, 1, 0, 0)
 
   // 创建水印层
   const bg = canvas.toDataURL('image/png')
-  const div = document.createElement('div')
+  const tags = 'a|b|div|em|i|p|small|span|u'.split('|')
+  const tag = tags[Math.floor(Math.random() * tags.length)]
+  const node = document.createElement(tag)
+  const id = `${tag}${new Date().getTime()}`
   const css = {
     display: 'block',
     margin: 0,
     padding: 0,
-    position: 'absolute',
+    position: 'fixed',
     top: 0,
     left: 0,
     right: 0,
@@ -49,6 +58,9 @@ export function watermark(textList = [], selector = 'body', width = 300, height 
     border: 0,
     opacity: 1,
     visibility: 'visible',
+    '-webkit-pointer-events': 'none',
+    '-moz-pointer-events': 'none',
+    '-ms-pointer-events': 'none',
     'pointer-events': 'none',
     'z-index': 99999
   }
@@ -56,22 +68,48 @@ export function watermark(textList = [], selector = 'body', width = 300, height 
   for (const key in css) {
     style += `${key}: ${css[key]}!important;`
   }
-  div.style = style
+  node.id = id
+  node.style = style
 
   // 加入水印层
-  const self = watermark
   const dom = document.querySelector(selector)
-  if (self.el) {
+  if (self._id) {
     // 尝试移除原来的水印层
+    // 先尝试通过id获取，id可能被修改，则使用缓存的数据
     try {
-      self.el.parentNode.removeChild(self.el)
+      const old = document.querySelector(`#${self._id}`) || self._el
+      self._el = null
+      // 如果dom节点被移除，会导致获取不到父节点
+      old.parentNode.removeChild(old)
     } catch (e) {
       console.log(e)
+      self._id = ''
+      self._el = null
     }
   }
-  dom.appendChild(div)
+  dom.appendChild(node)
+
   // 保存创建的元素，当再次调用时，需要清除，避免多次调用导致添加了多个水印层
-  self.el = div
+  self._id = id
+  self._el = node
+
+  // 防止水印被修改
+  function reDraw(e) {
+    // console.log(e)
+    const target = e.target || e[0].target
+    // 清理事件绑定，避免重复触发
+    ob.disconnect()
+    target.removeEventListener('DOMNodeRemoved', reDraw)
+    // 重要判断，防止死循环
+    self._el && self(options)
+  }
+  // 节点移除监听 DOMNodeRemoved 为 DOM level 3 规范
+  node.addEventListener('DOMNodeRemoved', reDraw)
+  // 节点属性修改为 DOM level 4 规范，使用 MutationObserver 来创建
+  const MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver
+  const ob = new MutationObserver(reDraw)
+  ob.observe(node, { attributes: true })
+
   // 释放内存
   setTimeout(() => {
     context = undefined
