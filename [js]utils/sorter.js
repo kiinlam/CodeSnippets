276466592d531099ea067b9490002edb73e84f54
kiinlam <516523522@qq.com>
2024-08-07 16:50:02 +0800

    Update sorter.js

--- [js]utils/sorter.js
+++ [js]utils/sorter.js
@@ -4,7 +4,85 @@ function pinyinSort(array) {
   return array.sort(sorter)
 }
 
-/**
+/*****************************************************************
+参考：https://zhuanlan.zhihu.com/p/599058139
+*/
+const chinese = ['12', '112', 'dea', 'abs', '中国', '北京', '天津', '上海', '重庆', 'edea', '中a', '中国q', '北京1', '北京2', '北京12'];
+const isLetterOrNumberReg = (str) => {
+    return /^[0-9a-zA-Z]+$/.test(str);
+}
+const isAllChineseStr = (str) => {
+    return /^[\u4E00-\u9FA5]+$/.test(str);
+};
+const splitStringByNumber = (str, sortByNumericalSize = false) => {
+  let strArr = [];
+  const REG_STRING_NUMBER_PARTS = /\d+|\D+/g;
+  const arr = str.match(REG_STRING_NUMBER_PARTS);
+  for (let i = 0; i < arr.length; i++) {
+    const splitStr = arr[i];
+    if (isNaN(splitStr)) {
+      strArr = strArr.concat(splitStr.split(''));
+    } else {
+      // Whether to split numbers
+      if (!sortByNumericalSize) {
+        strArr = strArr.concat(splitStr.split(''));
+      } else {
+        strArr.push(splitStr);
+      }
+    }
+  }
+  return strArr;
+};
+const compare = (a, b, sortByNumericalSize = true) => {
+    // 都是数字或字母
+    if (isLetterOrNumberReg(a) && isLetterOrNumberReg(b)) {
+        return a.localeCompare(b, 'zh-Hans-CN', {numeric: true}); 
+    }
+
+    // 中文字符串自己比较
+    if (isAllChineseStr(a) && isAllChineseStr(b)) {
+        return a.localeCompare(b, 'zh-Hans-CN', {numeric: true});
+    }
+
+    const arrA = splitStringByNumber(a, sortByNumericalSize);
+    const arrB = splitStringByNumber(b, sortByNumericalSize);
+
+    let result = 0;
+    const length = Math.min(arrA.length, arrB.length);
+    for (let i = 0; i < length; i++) {
+        const charA = arrA[i];
+        const charB = arrB[i];
+        // 数字, 字符串排在 中文前面
+        if (!isAllChineseStr(charA) && isAllChineseStr(charB)) {
+            return -1;
+        }
+
+        if (isAllChineseStr(charA) && !isAllChineseStr(charB)) {
+            return 1;
+        }
+
+        // 中文字符直接比较
+        if (isAllChineseStr(charA) && isAllChineseStr(charB)) {
+            result = charA.localeCompare(charB, 'zh-Hans-CN');
+        } else {
+            // 都不是中文
+            result = charA.localeCompare(charB, 'zh-Hans-CN', {numeric: true});
+        }
+
+
+        if (result !== 0) {
+            return result;
+        }
+    }
+
+    // result === 0;
+    if (arrA.length > arrB.length) return 1;
+    if (arrA.length < arrB.length) return -1;
+    return 0;
+}
+chinese.sort((a, b) => compare(a, b, true));
+
+/*****************************************************************
 参考
 原文链接：https://blog.csdn.net/qq_43840793/article/details/120492752
 思路
