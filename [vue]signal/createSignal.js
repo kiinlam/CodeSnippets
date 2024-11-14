d52e80876b1538ce69f479169a71add8c60d71c0
kiinlam <516523522@qq.com>
2024-11-14 10:49:50 +0800

    Create createSignal.js

---
+++ [vue]signal/createSignal.js
@@ -0,0 +1,11 @@
+import { shallowRef, triggerRef } from 'vue'
+
+export function createSignal(value, options) {
+  const r = shallowRef(value)
+  const get = () => r.value
+  const set = (v) => {
+    r.value = typeof v === 'function' ? v(r.value) : v
+    if (options?.equals === false) triggerRef(r)
+  }
+  return [get, set]
+}
